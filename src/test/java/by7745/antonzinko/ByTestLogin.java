edd323edcc4911a7892ed3d524e48911eb29d6c0
ZinkoAnton <antonzinko87@gmail.com>
2024-09-10 10:36:59 +0300

    Update ByTestLogin.java

--- src/test/java/by7745/antonzinko/ByTestLogin.java
+++ src/test/java/by7745/antonzinko/ByTestLogin.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class ByTestSite {
+public class ByTestLogin {
 
     @Test
     public void testLogin() {
