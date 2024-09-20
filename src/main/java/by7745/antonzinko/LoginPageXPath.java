de4497c800ae3ba05fe2d8bbcd8d13924ecfc41a
ZinkoAnton <antonzinko87@gmail.com>
2024-09-20 18:09:05 +0300

    Update LoginPageXPath.java

--- src/main/java/by7745/antonzinko/LoginPageXPath.java
+++ src/main/java/by7745/antonzinko/LoginPageXPath.java
@@ -1,6 +1,6 @@
 package by7745.antonzinko;
 
-public class LoginXPath {
+public class LoginPageXPath {
     public static final String INPUT_LOGIN_XPATH = "//div[@class='open-logon open-login']/div[1]/form[1]/div[1]/div[2]/input";
     public static final String INPUT_PASSWORD_XPATH = "//div[@class='open-logon open-login']/div[1]/form[1]/div[2]/input";
     public static final String BUTTON_ENTER_XPATH = "//div[@class='open-logon open-login']/div[1]/form[1]/div[4]/input[2]";
