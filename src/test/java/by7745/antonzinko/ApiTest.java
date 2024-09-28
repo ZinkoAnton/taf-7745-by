7f54aaaf0b256e4540723805a3bed0260b170e6a
ZinkoAnton <antonzinko87@gmail.com>
2024-09-28 21:42:32 +0300

    refactor ApiTest

--- src/test/java/by7745/antonzinko/ApiTest.java
+++ src/test/java/by7745/antonzinko/ApiTest.java
@@ -63,4 +63,49 @@ public class ApiTest {
                 then().
                 log().all();
     }
+
+    @Test
+    public void testEmptyData() {
+        String body1 = "{\"login\":\"\",\"password\":\"\"}";
+        String LOGIN_URL1 = "https://7745.by/login";
+        Map<String, String> headers1 = new HashMap<>();
+        headers1.put("Content-Type", "application/json");
+        given().
+                headers(headers1).
+                body(body1).
+                when().
+                post(LOGIN_URL1).
+                then().
+                log().all();
+    }
+
+    @Test
+    public void testEmptyLogin() {
+        String body2 = "{\"login\":\"\",\"password\":\"1\"}";
+        String LOGIN_URL2 = "https://7745.by/login";
+        Map<String, String> headers2 = new HashMap<>();
+        headers2.put("Content-Type", "application/json");
+        given().
+                headers(headers2).
+                body(body2).
+                when().
+                post(LOGIN_URL2).
+                then().
+                log().all();
+    }
+
+    @Test
+    public void testEmptyPassword() {
+        String body3 = "{\"login\":\"(1\",\"password\":\"\"}";
+        String LOGIN_URL3 ="https://7745.by/login";
+        Map<String, String> headers3 = new HashMap<>();
+        headers3.put("Content-Type", "application/json");
+        given().
+                headers(headers3).
+                body(body3).
+                when().
+                post(LOGIN_URL3).
+                then().
+                log().all();
+    }
 }
