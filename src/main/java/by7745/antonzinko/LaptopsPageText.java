719afa4afd268ca53b5fc48f51556f449b528b44
ZinkoAnton <antonzinko87@gmail.com>
2024-10-01 20:53:57 +0300

    add LaptopsPageText

---
+++ src/main/java/by7745/antonzinko/LaptopsPageText.java
@@ -0,0 +1,6 @@
+package by7745.antonzinko;
+
+public class LaptopsPageText {
+    public static final String LAPTOPS_PAGE_HEADER_PATH = "//h1[@class='bread-h1 catalog-header-news-articles']";
+    public static final String LAPTOPS_PAGE_HEADER_TEXT = "Ноутбуки";
+}
